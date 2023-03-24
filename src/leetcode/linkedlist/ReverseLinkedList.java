bf9599db15656705e34d11b55791ea5453639875
John DeMastri <demastri@msoe.edu>
2023-03-24 11:01:52 -0500

    ReverseLinkedList complete (recursive and non-recursive)

--- src/leetcode/linkedlist/ReverseLinkedList.java
+++ src/leetcode/linkedlist/ReverseLinkedList.java
@@ -1,43 +1,69 @@
 package leetcode.linkedlist;
 
+/**
+ * This class implements two versions
+ * - recursive (reverseList()) and non-recursive (reverseListNotRecursive())
+ * - both algorithms were interesting enough to include here, both are O(n)
+ * - they both center on isolated each pair and reversing the 1 link between them
+ */
 public class ReverseLinkedList {
 
-  /**
-   * Definition for singly-linked list. Given...
-   */
-  public static class ListNode {
-
-    int val;
-    ListNode next;
-
-    ListNode( ) {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverse(head, head.next);
+        head.next = null;
+        return newHead;
     }
 
-    ListNode( int val ) {
-      this.val = val;
+    private ListNode reverse(ListNode h, ListNode n) {
+        ListNode possHead = n;
+        if (n.next != null) {
+            possHead = reverse(n, n.next);
+        }
+        n.next = h;
+        return possHead;
     }
 
-    ListNode( int val, ListNode next ) {
-      this.val = val;
-      this.next = next;
-    }
-  }
+    public ListNode reverseListNotRecursive(ListNode head) {
+        ListNode p = null;
+        ListNode q = null;
+        ListNode r = head;
 
-    public ListNode reverseList( ListNode head ) {
-      if ( head == null || head.next == null ) {
-        return head;
-      }
-      return reverse( head, head.next );
+        // r is the farthest right node processed, q is prev, p is prev-prev
+        // if we have q and r, we need to set q to point to p (null first time)
+        // when r is null, after setting q to point to p, q is our new head
+
+        while (r != null) {
+            p = q;
+            q = r;
+            r = r.next; // order is important = need to update r before q (it points to THIS r...)
+            q.next = p;
+        }
+        return q;
     }
 
-    private ListNode reverse( ListNode h, ListNode n ) {
-      ListNode possHead = n;
-      if ( n.next != null ) {
-        possHead = reverse( n, n.next );
-      } else {
-        h.next = null;
-      }
-      n.next = h;
-      return possHead;
+    /**
+     * Definition for singly-linked list. Given...
+     */
+    public static class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
+
+
 }
--- test/leetcode/linkedlist/LinkedListTest.java
+++ test/leetcode/linkedlist/LinkedListTest.java
@@ -1,47 +1,85 @@
 package leetcode.linkedlist;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class LinkedListTest {
 
-  @Test
-  public void reverseListTest() {
-    ReverseLinkedList o = new ReverseLinkedList();
+    @Nested
+    @DisplayName("linked list reverse tests")
+    class ReverseTest {
+        @Test
+        public void reverseListTestRecursive() {
+            ReverseLinkedList o = new ReverseLinkedList();
 
-    ReverseLinkedList.ListNode n = null;
-    n = o.reverseList( n );
-    Assertions.assertNull( n );
+            ReverseLinkedList.ListNode n = null;
+            n = o.reverseList(n);
+            Assertions.assertNull(n);
 
-    n = new ReverseLinkedList.ListNode(2);
-    n = new ReverseLinkedList.ListNode(1, n);
-    n = o.reverseList( n );
-    Assertions.assertEquals( 2, n.val );
-    n = n.next;
-    Assertions.assertEquals( 1, n.val );
-    n = n.next;
-    Assertions.assertNull( n );
+            n = new ReverseLinkedList.ListNode(2);
+            n = new ReverseLinkedList.ListNode(1, n);
+            n = o.reverseList(n);
+            Assertions.assertEquals(2, n.val);
+            n = n.next;
+            Assertions.assertEquals(1, n.val);
+            n = n.next;
+            Assertions.assertNull(n);
 
 
-    n = new ReverseLinkedList.ListNode(5);
-    n = new ReverseLinkedList.ListNode(4, n);
-    n = new ReverseLinkedList.ListNode(3, n);
-    n = new ReverseLinkedList.ListNode(2, n);
-    n = new ReverseLinkedList.ListNode(1, n);
-    n = o.reverseList( n );
-    Assertions.assertEquals( 5, n.val );
-    n = n.next;
-    Assertions.assertEquals( 4, n.val );
-    n = n.next;
-    Assertions.assertEquals( 3, n.val );
-    n = n.next;
-    Assertions.assertEquals( 2, n.val );
-    n = n.next;
-    Assertions.assertEquals( 1, n.val );
-    n = n.next;
-    Assertions.assertNull( n );
-  }
+            n = new ReverseLinkedList.ListNode(5);
+            n = new ReverseLinkedList.ListNode(4, n);
+            n = new ReverseLinkedList.ListNode(3, n);
+            n = new ReverseLinkedList.ListNode(2, n);
+            n = new ReverseLinkedList.ListNode(1, n);
+            n = o.reverseList(n);
+            Assertions.assertEquals(5, n.val);
+            n = n.next;
+            Assertions.assertEquals(4, n.val);
+            n = n.next;
+            Assertions.assertEquals(3, n.val);
+            n = n.next;
+            Assertions.assertEquals(2, n.val);
+            n = n.next;
+            Assertions.assertEquals(1, n.val);
+            n = n.next;
+            Assertions.assertNull(n);
+        }
+        @Test
+        public void reverseListTestNotRecursive() {
+            ReverseLinkedList o = new ReverseLinkedList();
+
+            ReverseLinkedList.ListNode n = null;
+            n = o.reverseListNotRecursive(n);
+            Assertions.assertNull(n);
+
+            n = new ReverseLinkedList.ListNode(2);
+            n = new ReverseLinkedList.ListNode(1, n);
+            n = o.reverseListNotRecursive(n);
+            Assertions.assertEquals(2, n.val);
+            n = n.next;
+            Assertions.assertEquals(1, n.val);
+            n = n.next;
+            Assertions.assertNull(n);
+
+
+            n = new ReverseLinkedList.ListNode(5);
+            n = new ReverseLinkedList.ListNode(4, n);
+            n = new ReverseLinkedList.ListNode(3, n);
+            n = new ReverseLinkedList.ListNode(2, n);
+            n = new ReverseLinkedList.ListNode(1, n);
+            n = o.reverseListNotRecursive(n);
+            Assertions.assertEquals(5, n.val);
+            n = n.next;
+            Assertions.assertEquals(4, n.val);
+            n = n.next;
+            Assertions.assertEquals(3, n.val);
+            n = n.next;
+            Assertions.assertEquals(2, n.val);
+            n = n.next;
+            Assertions.assertEquals(1, n.val);
+            n = n.next;
+            Assertions.assertNull(n);
+        }
+    }
 
 }
